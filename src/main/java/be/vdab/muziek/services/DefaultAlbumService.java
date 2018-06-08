13dbc711612caec124cd659d495b1823727c02d8
Kristof Vermeulen <kristofvermeulen1980@gmail.com>
2018-06-08 15:31:21 +0200

    transaction isolation level vergeten

--- src/main/java/be/vdab/muziek/services/DefaultAlbumService.java
+++ src/main/java/be/vdab/muziek/services/DefaultAlbumService.java
@@ -4,11 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 import be.vdab.muziek.entities.Album;
 import be.vdab.muziek.repositories.AlbumRepository;
 
 @Service
+@Transactional(readOnly = true, isolation = Isolation.READ_COMMITTED)
 class DefaultAlbumService implements AlbumService {
 	private AlbumRepository repository;
 
