cbbb24e807e836b14a296e401c91716c77098453
Kristof Vermeulen <kristofvermeulen1980@gmail.com>
2018-06-08 11:14:51 +0200

    start project muziek

---
+++ src/main/java/be/vdab/muziek/MuziekApplication.java
@@ -0,0 +1,12 @@
+package be.vdab.muziek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuziekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuziekApplication.class, args);
+	}
+}
