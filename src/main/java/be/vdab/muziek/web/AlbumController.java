d71afe9bf8d6869f74efcc7441ba35bc7469338b
Kristof <Kristof@192.168.0.129>
2018-06-10 20:53:46 +0200

    nu ook met redirect

--- src/main/java/be/vdab/muziek/web/AlbumController.java
+++ src/main/java/be/vdab/muziek/web/AlbumController.java
@@ -1,25 +1,35 @@
 package be.vdab.muziek.web;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import be.vdab.muziek.entities.Album;
 import be.vdab.muziek.services.AlbumService;
 
 @Controller
 @RequestMapping("/album")
 class AlbumController {
 	private final AlbumService albumService;
-	private final String VIEW = "album";
+	private final static String VIEW = "album";
+	private final static String REDIRECT_ALBUM_NIET_GEVONDEN = "redirect:/";
 
 	public AlbumController(AlbumService albumService) {
 		this.albumService = albumService;
 	}
 	
 	@GetMapping("{id}")
-	ModelAndView album(@PathVariable long id) {
-		return new ModelAndView(VIEW,"album",albumService.read(id).get());
+	ModelAndView album(@PathVariable long id, RedirectAttributes redirectAttributes) {
+		Optional<Album> album = albumService.read(id);
+		if (album.isPresent()) {
+			return new ModelAndView(VIEW,"album",album.get());
+		}
+		redirectAttributes.addAttribute("fout","album niet gevonden");
+		return new ModelAndView(REDIRECT_ALBUM_NIET_GEVONDEN);
 	}
 }
--- src/main/webapp/WEB-INF/JSP/index.jsp
+++ src/main/webapp/WEB-INF/JSP/index.jsp
@@ -7,14 +7,17 @@
 	<title>Startpagina muziek</title>
 </head>
 	<body>
+		<c:if test="${not empty param.fout}">
+			<div>${param.fout}</div>
+		</c:if>
 		<h1>Kies je album:</h1>
 		<ul>
 			<c:forEach var='album' items='${albums}'>
+				<spring:url value='/album/{id}' var='url'>   
+					<spring:param name='id' value='${album.id}'/> 
+				</spring:url> 
 				<li>
-					<spring:url value='/album/{id}' var='url'>   
-						<spring:param name='id' value='${album.id}'/> 
-					</spring:url> 
-					<a href='${url}'>${album.naam}</a> 
+					<a href='${url}'>${album.naam}</a> (${album.artiest.naam})
 				</li>
 			</c:forEach>
 		</ul>
